1213b0861127bbadc03dcd31e98b8b1f555791ae
rampazo <113611852+kahz1nn@users.noreply.github.com>
2025-04-08 15:24:06 -0300

    AUTERAÇAO 2

---
+++ EX3.c
@@ -0,0 +1,30 @@
+/*
+nome:.... KAIO GABRIEL DA SILVA RAMPAZO
+turma:.... EC41A-C11
+data:.... 08/04/2025
+Enunciado:...Faça um programa em Linguagem C que peça uma temperatura em graus 
+célsius e apresente seu respectivo valor em fahrenheit. O cálculo de conversão é: F = (9 * C / 5) + 32
+*/
+#include <stdio.h>
+
+int main(){
+    
+    //declaraçao de variaveis
+    printf("Hello World");
+    float cell = 0,
+        farin = 0;
+
+   // entrada
+   printf("ou, me passa um valor em celsius");
+   scanf("%f", &cell);
+   
+   //processamento
+   farin = (9 * cell / 5) + 32;
+   
+   //saida
+   printf (" aqui o valor do Fahrenheit = %.2f", farin);
+   
+   
+    return 0;
+}
+
---
+++ EX5.c
@@ -0,0 +1,42 @@
+ /*
+nome..: KAIO GABRIEL DA SILVA RAMPAZO
+turma..:EC41A-C11
+data..: 08/04/2025
+Enunciado:....FIX01_EX05: Elabore um programa em Linguagem C que calcule o preço de venda de um carro.
+ O preço de venda é formado pelo preço da montadora, mais 15% de lucro, mais 11% de IPI, mais 17% de ICMS. 
+ As porcentagens são sobre o preço da montadora, que é lido do usuário. Apresente na tela o preço final, 
+ o lucro e o valor dos impostos. 
+Tente apresentar todos os dados no formato de uma tabela, com valores alinhados.
+*/
+ 
+#include <stdio.h>
+
+ int main(){
+// DECLARAÇAO DA VARIAVEIS
+     float vl_montadora = 0,
+           vl_final = 0,
+           vl_ipi = 0,
+           vl_icms = 0,
+           vl_lucro = 0;
+
+// entrada
+    printf("Calculos dos Malores na Montadoras \n");
+    printf("digite o valor:");
+    scanf("%f", &vl_montadora);
+
+//processor
+   vl_ipi = vl_montadora * 0.11;
+   vl_icms = vl_montadora * 0.17;
+   vl_lucro = vl_montadora * 0.15;
+   vl_final = vl_ipi + vl_icms + vl_lucro + vl_montadora;
+
+
+// saida
+   printf("\n| IPI.........: R$ %10.2f |", vl_ipi);
+   printf("\n| ICMS........: R$ %10.2f |", vl_icms);
+   printf("\n| Lucro.......: R$ %10.2f |", vl_lucro);
+   printf("\n| PREÇO FINAL.: R$ %10.2f |", vl_final);
+
+
+return 0;
+ }
